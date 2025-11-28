0daa5f5d8fb013aa066d7a9580f263a69325e5d9
Min-code0202 <jjm001115@gmail.com>
2025-11-28 12:52:49 +0900

    복원

---
+++ src/main/java/com/ssafy/zipcheck/ZipcheckApplication.java
@@ -0,0 +1,13 @@
+package com.ssafy.zipcheck;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZipcheckApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ZipcheckApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ssafy/zipcheck/ZipcheckApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ssafy.zipcheck;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZipcheckApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
